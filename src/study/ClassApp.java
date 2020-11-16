9bc9b34b3419f4b762c6ed9b987e237c83ddc628
yoonsi <yunsell31@gmail.com>
2020-11-17 01:03:30 +0900

    java1-14.1

---
+++ src/study/ClassApp.java
@@ -0,0 +1,11 @@
+package study;
+
+public class ClassApp {
+
+	public static void main(String[] args) {
+		
+		System.out.println(Math.PI);
+		System.out.println(Math.floor(1.6)); //버림
+		System.out.println(Math.ceil(1.6));	 //올림
+	}
+}
